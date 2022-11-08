c7a5c096c40fff7bb32027f74e31189aaa8bbf23
MonaBlanc <jacquemotkimberley@gmail.com>
2022-11-08 11:25:51 +0100

    Push Joined Branches

--- src/components/RandomMeal/RandomList.jsx
+++ src/components/RandomMeal/RandomList.jsx
@@ -31,9 +31,9 @@ const RandomList = () => {
                 <div className="row">
                     <div className="columns five">
                         <img src={meal.strMealThumb} alt="Meal Image" />
-                        {meal.strCategory ? `<p><strong>Category:</strong> {meal.strCategory}</p>` : ''}
-                        {meal.strArea ? `<p><strong>Area:</strong> ${meal.strArea}</p>` : ''}
-                        {meal.strTags ? `<p><strong>Tags:</strong> ${meal.strTags.split(',').join(', ')}</p>` : ''}
+                        {meal.strCategory ? <p><strong>Category:</strong> {meal.strCategory}</p> : ''}
+                        {meal.strArea ? <p><strong>Area:</strong> ${meal.strArea}</p> : ''}
+                        {meal.strTags ? <p><strong>Tags:</strong> ${meal.strTags.split(',').join(', ')}</p> : ''}
                         <h5>Ingredients:</h5>
                         <ul>
                             {ingredients.map(ingredient => `<li>{ingredient}</li>`).join('')}
