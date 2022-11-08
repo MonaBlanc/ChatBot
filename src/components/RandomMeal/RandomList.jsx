48fe1e4432e8beaf439acc301f595ff3c42d622d
Ayoub <ayoub.echchamali@ilium.co>
2022-11-08 11:27:22 +0100

    modifs

--- src/components/RandomMeal/RandomList.jsx
+++ src/components/RandomMeal/RandomList.jsx
@@ -32,8 +32,8 @@ const RandomList = () => {
                     <div className="columns five">
                         <img src={meal.strMealThumb} alt="Meal Image" />
                         {meal.strCategory ? <p><strong>Category:</strong> {meal.strCategory}</p> : ''}
-                        {meal.strArea ? <p><strong>Area:</strong> ${meal.strArea}</p> : ''}
-                        {meal.strTags ? <p><strong>Tags:</strong> ${meal.strTags.split(',').join(', ')}</p> : ''}
+                        {meal.strArea ? <p><strong>Area:</strong> {meal.strArea}</p> : ''}
+                        {meal.strTags ? <p><strong>Tags:</strong> {meal.strTags.split(',').join(', ')}</p> : ''}
                         <h5>Ingredients:</h5>
                         <ul>
                             {ingredients.map(ingredient => `<li>{ingredient}</li>`).join('')}
@@ -41,7 +41,7 @@ const RandomList = () => {
                     </div>
                     <div className="columns seven">
                         <h4>{meal.strMeal}</h4>
-                        <p>{item.strInstructions}</p>
+                        <p>{meal.strInstructions}</p>
                     </div>
                 </div>
             }
