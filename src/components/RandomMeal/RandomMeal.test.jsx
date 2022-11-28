da8bd97fe8007d1979e58a2dbf2ce87c88a5438f
MonaBlanc <jacquemotkimberley@gmail.com>
2022-11-28 10:55:28 +0100

    Rendu avec tests Génie Logiciel

--- src/components/RandomMeal/RandomMeal.test.jsx
+++ src/components/RandomMeal/RandomMeal.test.jsx
@@ -1,12 +1,20 @@
-/*import axios from 'axios';
-import RandomMeal from './RandomMeal';
+import RandomList from './RandomMeal.test.jsx';
+import axios from 'axios';
 
 jest.mock('axios');
 
+describe("RandomMeal", () => {
+  describe("when API call is successful", () => {
+    it("should return meal details", () => Promise.resolve({ status: 200 }));
+    });
+  });
+
+
 test('should fetch ingredients', () => {
-  const ingredients = [{name: 'ingredient'}];
-  const resp = {meal: ingredients};
+  const meal = [{ingredients: ''}];
+  const resp = {data: meal};
   axios.get.mockResolvedValue(resp);
 
-  return RandomMeal.all().then(meal => expect(meal).toEqual(ingredients));
-});*/
+
+  return RandomList.getIngredients().then(data => expect(data).toEqual(meal));
+});
