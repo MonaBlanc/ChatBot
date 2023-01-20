3de32539d1f647b652d00d1f868a9ceff35b9658
draxx023 <ayoub2001@hotmail.it>
2023-01-20 10:25:57 +0000

    chatbot page front

--- src/components/Pages/molecules/Header.jsx
+++ src/components/Pages/molecules/Header.jsx
@@ -3,10 +3,10 @@ import { Link } from 'react-router-dom';
 
 export default function Header() {
     return (
-        <header className="body-font font-black">
+        <header className="body-font font-black" style={{height: '11vh'}}>
             <nav className="container mx-auto flex text-center flex-row justify-around p-5 md:flex-row items-center">
                 <Link to="/" className="flex items-center text-gray-900">
-                    <span className="text-xl">Logo Chatbot</span>
+                    <span className="text-xl">Logo Chefbot</span>
                 </Link>
                 <Link to="/" className="hover:text-gray-600">Home</Link>
                 <Link to="#" className="text-orange hover:text-lightOrange">Fridge Icon</Link>
--- src/components/Recipe/Recipe.jsx
+++ src/components/Recipe/Recipe.jsx
@@ -9,12 +9,12 @@ const Recipe = (props) => {
     return (
         <>
             {
-                <div className="infos row">
-                    <div className="columns seven">
-                        <h3 className="text-xl text-orange">{meal.strMeal}</h3>
+                <div className="infos-recipe row">
+                    <img className="thumb" src={meal.strMealThumb} alt="Meal Image" />
+                    <div className="columns seven recipe-title">
+                        <h3 className="text-xl">{meal.strMeal}</h3>
                     </div>
                     <div className="columns five">
-                        <img src={meal.strMealThumb} alt="Meal Image" />
                         {meal.strCategory ? <p><strong>Category:</strong> {meal.strCategory}</p> : ''}
                         {meal.strArea ? <p><strong>Area:</strong> {meal.strArea}</p> : ''}
                         {meal.strTags ? <p><strong>Tags:</strong> {meal.strTags.split(',').join(', ')}</p> : ''}
