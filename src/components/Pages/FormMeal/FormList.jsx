47053d0d806855af4331caac55fa3e1cafae09ae
Ayoub <ayoub.echchamali@ilium.co>
2023-01-29 16:11:28 +0100

    img fix

--- src/components/Pages/FormMeal/FormList.jsx
+++ src/components/Pages/FormMeal/FormList.jsx
@@ -29,7 +29,7 @@ const FormList = (props) => {
                     <div className="columns seven">
                         <h3 className="text-xl">{meal.strMeal}</h3>
                     </div>
-                    <img src={meal.strMealThumb} alt="img-thumb" class="img-thumb" />
+                    <img src={meal.strMealThumb} alt="img-thumb" class="img-thumb-random" />
                     {meal.strCategory ? <p id="mealName"><strong>Category:</strong> {meal.strCategory}</p> : ''}
                     {meal.strArea ? <p id="mealArea"><strong>Area:</strong> {meal.strArea}</p> : ''}
                     {meal.strTags ? <p id="mealTag"><strong>Tags:</strong> {meal.strTags.split(',').join(', ')}</p> : ''}
--- src/components/Pages/RandomMeal/RandomList.jsx
+++ src/components/Pages/RandomMeal/RandomList.jsx
@@ -26,7 +26,7 @@ const RandomList = (props) => {
                     <div className="columns seven">
                         <h3 className="text-xl">{meal.strMeal}</h3>
                     </div>
-                    <img src={meal.strMealThumb} alt="meal-thumb" className="img-thumb" />
+                    <img src={meal.strMealThumb} alt="meal-thumb" className="img-thumb-random" />
                     {meal.strCategory ? <p id="mealName"><strong>Category:</strong> {meal.strCategory}</p> : ''}
                     {meal.strArea ? <p id="mealArea"><strong>Area:</strong> {meal.strArea}</p> : ''}
                     {meal.strTags ? <p id="mealTags"><strong>Tags:</strong> {meal.strTags.split(',').join(', ')}</p> : ''}
