1b949c07871e8ec100d5281f5e589a3a8316535a
MonaBlanc <kim.mauritius@gmail.com>
2023-05-31 11:01:05 +0200

    Chat is responsive, need to look for the components tho

--- src/components/Pages/molecules/Header.jsx
+++ src/components/Pages/molecules/Header.jsx
@@ -19,7 +19,7 @@ export default function Header() {
           <button
             data-collapse-toggle="navbar-default"
             type="button"
-            className="inline-flex items-center p-2 ml-3 text-sm text-gray-500 rounded-lg md:hidden hover:bg-gray-100 focus:outline-none focus:ring-2 focus:ring-gray-200 dark:text-gray-400 dark:hover:bg-gray-700 dark:focus:ring-gray-600"
+            className="inline-flex items-center p-2 ml-3 text-sm text-orange-500 rounded-lg md:hidden hover:bg-orange-100 focus:outline-none focus:ring-2 focus:ring-orange-200 dark:text-orange-400 dark:hover:bg-orange-700 dark:focus:ring-orange-600"
             aria-controls="navbar-default"
             aria-expanded="false"
           >
