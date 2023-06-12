214594650b7d7b935aabbaa5c06aef32a5a356ab
draxx023 <ayoub2001@hotmail.it>
2023-06-12 12:00:33 +0200

    deploy fix

--- src/components/Pages/molecules/LoginForm.jsx
+++ src/components/Pages/molecules/LoginForm.jsx
@@ -14,9 +14,6 @@ export default function LoginForm(props) {
   let { handleSubmit, setEmail, setPassword, errorMessage, setError } =
     props.loginState;
   const [passwordShown, setPasswordShown] = useState(false);
-  const location = useLocation();
-  let defaultClass = "nav-link link-btn btn-primary text-gray-600 opacity-70 text-sm";
-  let active = " active"
 
   // Password toggle handler
   const togglePassword = () => {
